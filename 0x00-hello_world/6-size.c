b9ed2e86203be56ade7c2e2bdb5fc5fdca75471b
Iggychah <igchazuka@gmail.com>
2022-09-08 18:22:16 -0700

    c_first-attempt

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+        int i;                                  char c;
+	long li;
+	long long lli;
+	float f;
+   	printf("Size of a char: %ld.\n", sizeof(c));                                     printf("Size of an int: %ld.\n", sizeof(i));                                      printf("Size of a long int: %ld.\n", sizeof(li));
+	     	printf("Size of a long long int: %ld.\n", sizeof(lli));
+	;                                     printf("Size of a float: %ld.\n", sizeof(f));	
+ return (0);
+
+}
