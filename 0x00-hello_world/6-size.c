5001df2bb3dddf15d53854c41dd80d016049346c
Iggychah <igchazuka@gmail.com>
2022-09-08 18:53:19 -0700

    second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,12 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-   	printf("Size of a char: %ld.\n", sizeof(c));                                     printf("Size of an int: %ld.\n", sizeof(i));                                      printf("Size of a long int: %ld.\n", sizeof(li));
-	     	printf("Size of a long long int: %ld.\n", sizeof(lli));
-	;                                     printf("Size of a float: %ld.\n", sizeof(f));	
- return (0);
+
+   	printf("Size of a char: %ld.\n", sizeof(c));
+	printf("Size of an int: %ld.\n", sizeof(i));
+	printf("Size of a long int: %ld.\n", sizeof(li));
+        printf("Size of a long long int: %ld.\n", sizeof(lli));
+        printf("Size of a float: %ld.\n", sizeof(f));	
+             return (0);
 
 }
