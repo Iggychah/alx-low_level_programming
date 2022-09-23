339c2995a0bb179e3ed8e3745c7a1c65299d4157
Iggychah <igchazuka@gmail.com>
2022-09-23 08:18:49 -0700

    0x06-first-commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+
+#ifndef myheade_filer
+#define myheader_file
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *leet(char *s);
+int _strtotal(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+int is_seperators(char c);
+char *rot13(char *s);
+#endif
